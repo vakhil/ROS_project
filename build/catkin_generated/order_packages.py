4e916314754a8b537c522740cb04b97f0ba8343c
Vemula Akhil <akhilvemula95@gmail.com>
2017-04-18 01:09:03 +0530

    Laser working perfectly

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/vemula/Desktop/mybot_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/vemula/Desktop/mybot_ws/devel;/home/vemula/mybot_ws/devel;/opt/ros/kinetic".split(';') if "/home/vemula/Desktop/mybot_ws/devel;/home/vemula/mybot_ws/devel;/opt/ros/kinetic" != "" else []
